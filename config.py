9a33612b5052cad24056c8031d8155d45f38e153
AddieBaraiya <baraiyaaditi14@gmail.com>
2025-05-04 10:57:51 +0530

    Add Gimini API key

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # todo: Add your api key here
-GEMINI_API_KEY = "AIzaSyCX7Kw5Y7sdUqfex2DdH8o5P5L5vEp7pNs"  # Replace with your actual API key
+GEMINI_API_KEY = "#"  # Replace with your actual API key
 apikeys = GEMINI_API_KEY
